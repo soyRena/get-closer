1c40e37445665b26b0d866000cebe24313231ec8
soyRena <rennanfelipers@gmail.com>
2023-11-13 14:23:31 -0300

    refactor: update navigation menu icons

--- src/navigation-menu/components/navigation-menu-item.tsx
+++ src/navigation-menu/components/navigation-menu-item.tsx
@@ -10,11 +10,9 @@ export function NavigationMenuItem(props: NavigationMenuItemProps) {
   const { goTo, icon, label } = props
 
   return (
-    <li>
-      <Link href={goTo} className="flex items-center p-5 text-gray-900 rounded-lg hover:bg-gray-100 group">
-        {icon}
-        <span className="flex-1 ml-3 whitespace-nowrap">{label}</span>
-      </Link>
-    </li>
+    <Link href={goTo} className="flex items-center p-5 text-gray-900 rounded-full hover:bg-gray-100 group">
+      {icon}
+      <span className="flex-1 ml-3 whitespace-nowrap">{label}</span>
+    </Link>
   )
 }
--- src/navigation-menu/components/navigation-menu.tsx
+++ src/navigation-menu/components/navigation-menu.tsx
@@ -1,71 +1,38 @@
-import { AddressIcon } from '~/shared/assets/address-icon'
-import { DashboardIcon } from '../../shared/assets/dashboard-icon'
-import { ContactIcon } from '~/shared/assets/contact-icon'
 import { NavigationMenuItem } from './navigation-menu-item'
+import { LayoutGrid, Mails, Pin } from 'lucide-react'
 
 const itemsMap = [
   {
     label: 'Dashboard',
-    icon: <DashboardIcon />,
+    icon: <LayoutGrid fill="#2c2c2c" color="#2c2c2c" size="18" />,
     goTo: '#'
   },
   {
     label: 'Endereços',
-    icon: <AddressIcon />,
+    icon: <Pin fill="#2c2c2c" color="#2c2c2c" size="18" />,
     goTo: '#'
   },
   {
     label: 'Contato',
-    icon: <ContactIcon />,
+    icon: <Mails color="#2c2c2c" size="18" />,
     goTo: '#'
   }
 ]
 
 export function NavigationMenu() {
   return (
-    <>
-      <button
-        data-drawer-target="default-sidebar"
-        data-drawer-toggle="default-sidebar"
-        aria-controls="default-sidebar"
-        type="button"
-        className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-      >
-        <span className="sr-only">Open sidebar</span>
-        <svg
-          className="w-6 h-6"
-          aria-hidden="true"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            clip-rule="evenodd"
-            fill-rule="evenodd"
-            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-          ></path>
-        </svg>
-      </button>
-      
-      <aside
-        id="default-sidebar"
-        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
-        aria-label="Sidebar"
-      >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-zinc-50 space-y-11">
-          <div className="text-4xl font-bold">
-            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg">
-              <span className="ml-3">GetCloser</span>
-            </a>
-          </div>
+    <div className="h-full px-3 py-4 overflow-y-auto bg-zinc-50 space-y-11">
+      <nav className="text-4xl font-bold">
+        <a href="#" className="flex items-center p-2 text-gray-900 ml-3">
+          GetCloser
+        </a>
+      </nav>
 
-          <ul className="space-y-11 font-medium">
-            {itemsMap.map((item, index) => {
-              return <NavigationMenuItem label={item.label} icon={item.icon} goTo={item.goTo} key={index} />
-            })}
-          </ul>
-        </div>
-      </aside>
-    </>
+      <nav className="space-y-11 font-medium">
+        {itemsMap.map((item, index) => {
+          return <NavigationMenuItem label={item.label} icon={item.icon} goTo={item.goTo} key={index} />
+        })}
+      </nav>
+    </div>
   )
 }
