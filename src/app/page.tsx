251c7c846a423d4ee9fbc288b58c1958e49c1b1f
soyRena <rennanfelipers@gmail.com>
2023-12-08 17:43:02 -0300

    feat: check localstorage in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
+'use client'
+
 import { AddressSearchInput } from '~/components/address-search-input/address-search-input'
 import { EmptyAddressState } from '~/components/empty-address-state/empty-address-state'
 
 export default function Home() {
-  return (
-    <>
-      <AddressSearchInput />
-      <EmptyAddressState />
-    </>
-  )
+  const registeredAddress = window.localStorage.getItem('SAVED_ADDRESS')
+
+  return <>{Boolean(registeredAddress) ? <AddressSearchInput /> : <EmptyAddressState />}</>
 }
